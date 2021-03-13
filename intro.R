0c4a29c0bce44cc22c6cd0cff2ecdc12e69b4b06
unknown <samolix0@gmail.com>
2021-03-13 21:38:48 +0100

    adding missing column

--- intro.R
+++ intro.R
@@ -1,9 +1,18 @@
 
 
+install.packages("devtools")
+library(devtools)
+install_github("rcastelo/GSVA")
+
+
 
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("EnrichmentBrowser")
+
+
+install.packages("BiocManager")
+BiocManager::install("GSVA", version = "devel")
 
 BiocManager::install("AnnotationDbi")
 BiocManager::install("hgu133a.db")
@@ -15,6 +24,9 @@ BiocManager::install("KEGGREST")
 BiocManager::install("org.Hs.eg.db")
 BiocManager::install("GSEABenchmarkeR")
 BiocManager::install("DataCombine")
+
+BiocManager::install("BiocParallel")
+
 
 library("DataCombine")
 library("KEGGREST")
@@ -28,7 +40,11 @@ library("hgu133plus2.db")
 library("plyr")
 library("org.Hs.eg.db")
 
-setwd('C:/Users/hp/Desktop/mgr')
+library("BiocParallel")
+library('data.table')
+library("dplyr") 
+
+setwd('C:/Users/zelda/mgr')
 
 
 
@@ -109,7 +125,7 @@ listDatabases()
 hsa_list = keggList("hsa")
 
 
-data<-read.table("KEGG_GS.txt",sep='\t')
+data<-read.table("Info_files/KEGG_GS.txt",sep='\t')
 ## GeneSetCollection KEGGREST
 Gz<-list()
 for (i in 1:dim(data)[1]){
@@ -173,7 +189,8 @@ write.csv(unique_expr_symbols, "ds_expr_uni/ds_1_uniq.csv", row.names = TRUE)
 df_c_d <- data.frame(matrix(ncol = 3, nrow = 42))
 colnames(df_c_d) <- c("DataSet", "Diseases","Control")
 
-for (nset in 1:length(geo2kegg)){
+## for (nset in 1:length(geo2kegg)){
+for (nset in 1:2){
   ## controles / diseases
   dataset_tmp = geo2kegg[[nset]]
   c_d_data = dataset_tmp@phenoData@data
@@ -194,6 +211,7 @@ delete.na <- function(DF, n=0) {
 
 ## create dataset expr unique for each dataset --- update save with enterez id as rownames!!!!
 for (dset in 1:length(geo2kegg)){
+## for (dset in 1:2){
   dataset_tmp=geo2kegg[[dset]]
   dataset_tmp_expr = dataset_tmp@assayData$exprs
   probes_tmp<- as.character(dataset_tmp_expr[,1, drop=FALSE])
@@ -210,13 +228,14 @@ for (dset in 1:length(geo2kegg)){
   binded_tmp = as.data.frame(binded_tmp)
   
   ## order by var
-  order_var_tmp = binded_tmp[order(binded_tmp$ls_symbols_tmp, -(as.numeric(binded_tmp$rows_var)[as.numeric(binded_tmp$rows_var)])),]
+  ## order_var_tmp = binded_tmp[order(binded_tmp$ls_symbols_tmp, -(as.numeric(binded_tmp$rows_var)[as.numeric(binded_tmp$rows_var)])),]
+  order_var_tmp = binded_tmp[with(binded_tmp, order(ls_symbols_tmp,rows_var)),]
   ## remove duplicates
   unique_f_tmp = order_var_tmp[!duplicated(order_var_tmp$ls_symbols_tmp),]
-  file_name <- paste("ds_expr_enterez/ds_uniq", "_",dset,".csv", sep="")
+  file_name <- paste("Results/ds_expr_enterez2/ds_uniq", "_",dset,"_e.csv", sep="")
   unique_f_tmp = subset(unique_f_tmp, select=-c(rows_var))
-  unique_f_tmp = unique_f_tmp[,-2]
-  unique_f_tmp = delete.na(unique_f_tmp)
+  ## unique_f_tmp = unique_f_tmp[,-2]
+  unique_f_tmp = na.omit(unique_f_tmp)
   rownames(unique_f_tmp) = unique_f_tmp$ls_symbols_tmp
   unique_f_tmp[,1] = NULL
   write.csv(unique_f_tmp, file_name, row.names = TRUE)
@@ -228,7 +247,7 @@ for (dset in 1:length(geo2kegg)){
 
 
 ## load gene sets 
-data<-read.table("KEGG_GS.txt",sep='\t')
+data<-read.table("Info_files/KEGG_GS.txt",sep='\t')
 ## GeneSetCollection KEGGREST
 Gz<-list()
 for (i in 1:dim(data)[1]){
@@ -289,39 +308,43 @@ write.csv(test, "test.csv", row.names = TRUE)
 ### annotation:  "org.Hs.eg.db" 
 ### run for each method
 
-methods_gsva = c("gsva","ssgsea", "zscore", "plage")
+## methods_gsva = c( "zscore", "plage")
+methods_gsva = c("zscore")
+
 
 for (m in methods_gsva){
   
   for (i in 1:length(geo2kegg)){
     # read from file expression set: rows - enterezID, cols - genes
-    file_name <- paste("ds_expr_enterez/ds_uniq", "_",i,".csv", sep="")
+    file_name <- paste("Results/ds_expr_enterez2/ds_uniq", "_",i,"_e.csv", sep="")
     ds_tmp = read.csv(file_name,check.names=FALSE, row.names=1)
     # main function
     ES_GSVA = gsva(data.matrix(ds_tmp), gsc,
-                   method= m,
+                   method= "zscore",
                    kcdf="Gaussian",
                    abs.ranking=FALSE,
-                   min.sz=1,
+                   min.sz=2,
                    max.sz=Inf, ## all paths even if short
-                   parallel.sz=2,
+                   parallel.sz=1L,
                    mx.diff=TRUE,
                    ssgsea.norm=TRUE,
-                   verbose=TRUE)
+                   verbose=TRUE,
+                   BPPARAM=SnowParam(2, progressbar = FALSE))
     # write to apropriate folder
-    file_out <- paste(m, "/ds", "_",i,".csv", sep="")
+    file_out <- paste("Results/new_res/", m, "/ds", "_",i,"_e.csv", sep="")
     write.csv(ES_GSVA, file_out, row.names = TRUE)
   }
   m
 }
 
 
-
-ES_GSVA = gsva(data.matrix(ds1_uni), gsc,
-               method="gsva",
+file_name <- paste("Results/ds_expr_enterez2/ds_uniq_42_e.csv", sep="")
+ds_tmp = read.csv(file_name,check.names=FALSE, row.names=1)
+ES_GSVA = gsva(data.matrix(ds_tmp), gsc,
+               method="zscore",
                kcdf="Gaussian",
                abs.ranking=FALSE,
-               min.sz=1,
+               min.sz=2,
                max.sz=Inf, ## all paths even if short
                parallel.sz=1L,
                mx.diff=TRUE,
@@ -329,3 +352,148 @@ ES_GSVA = gsva(data.matrix(ds1_uni), gsc,
                verbose=TRUE)
 
 
+
+### calculate t-test
+
+
+## make df for each method
+
+gsva_ttest <- data.frame(matrix(ncol = 5, nrow = 42))
+colnames(gsva_ttest) <- c("DataSet", "t_value","p_value", 'c_nbr', 'd_nbr')
+
+plage_ttest <- data.frame(matrix(ncol = 5, nrow = 42))
+colnames(plage_ttest) <- c("DataSet", "t_value","p_value", 'c_nbr', 'd_nbr')
+
+ssgsea_ttest <- data.frame(matrix(ncol = 5, nrow = 42))
+colnames(ssgsea_ttest) <- c("DataSet", "t_value","p_value", 'c_nbr', 'd_nbr')
+
+zscore_ttest <- data.frame(matrix(ncol = 5, nrow = 42))
+colnames(zscore_ttest) <- c("DataSet", "t_value","p_value", 'c_nbr', 'd_nbr')
+
+path_1 = 'Results/ds_expr_enterez2'
+
+method_ttest <- function(df_ttest, path_f) {
+  for (nset in 1:length(geo2kegg)){
+  ##for (nset in 1:2){
+    ## controles / diseases
+    dataset_tmp = geo2kegg[[nset]]
+    c_d_data = dataset_tmp@phenoData@data
+    nbr_c = count(c_d_data, vars = "Group")
+    df_ttest$DataSet[nset] = nset
+    df_ttest$d_nbr[nset] = sum(c_d_data$Group=="d")
+    df_ttest$c_nbr[nset] = sum(c_d_data$Group=="c")
+    
+    t_df <- transpose(c_d_data)
+    colnames(t_df) <- c_d_data$Sample
+    
+    file_name <- paste(path_f, 'ds', "_",nset,"_e.csv", sep="")
+    ds_tmp = read.csv(file_name,check.names=FALSE, row.names=1)
+    c_sample = c_d_data$Sample[c_d_data$Group== 'c']
+    d_sample = c_d_data$Sample[c_d_data$Group== 'd']
+    c_mean = mean(as.matrix(ds_tmp[ , c(c_sample)]))
+    d_mean = mean(as.matrix(ds_tmp[ , c(d_sample)]))
+    
+    ## transpose 
+    t_tmp = transpose(ds_tmp)
+    colnames(t_tmp) <- rownames(ds_tmp)
+    rownames(t_tmp) = colnames(ds_tmp)
+    
+    ## calculate means for each row
+    t_tmp$Means = rowMeans(t_tmp[,-1])
+    t_tmp$Group = c_d_data$Group
+    
+    ##  t-test
+    df <- t_tmp %>%
+      filter(Group == "c" | Group == "d") %>%
+      select(Group, Means)
+    
+    t1 = t.test(Means ~ Group, data = df)
+    df_ttest$t_value[nset] = t1$statistic
+    df_ttest$p_value[nset] = t1$p.value
+    
+  }
+    
+  return(df_ttest)
+}
+
+
+
+gsva_ttest = method_ttest(gsva_ttest, 'Results/new_res/gsva/')
+plage_ttest = method_ttest(plage_ttest, 'Results/new_res/plage/')
+ssgsea_ttest = method_ttest(ssgsea_ttest, 'Results/new_res/ssgsea/')
+
+write.csv(gsva_ttest, 'Results/new_res/gsva_ttest.csv', row.names = TRUE)
+write.csv(plage_ttest, 'Results/new_res/plage_ttest.csv', row.names = TRUE)
+write.csv(ssgsea_ttest, 'Results/new_res/ssgsea_ttest.csv', row.names = TRUE)
+
+
+
+df_ttest = gsva_ttest
+path_f = 'Results/new_res/gsva/'
+
+
+
+
+gsva_pval <- data.frame(matrix(ncol = 1, nrow = 1))
+colnames(gsva_pval) = ('genes')
+
+
+
+t_test_all_genes <- function(path_f) {
+gsva_pval <- data.frame(matrix(ncol = 1, nrow = 1))
+colnames(gsva_pval) = c('genes')
+
+for (nset in 1:length(geo2kegg)){
+  ##for (nset in 1:2){
+  ## controles / diseases
+  dataset_tmp = geo2kegg[[nset]]
+  c_d_data = dataset_tmp@phenoData@data
+  nbr_c = count(c_d_data, vars = "Group")
+  df_ttest$DataSet[nset] = nset
+  df_ttest$d_nbr[nset] = sum(c_d_data$Group=="d")
+  df_ttest$c_nbr[nset] = sum(c_d_data$Group=="c")
+  
+  t_df <- transpose(c_d_data)
+  colnames(t_df) <- c_d_data$Sample
+  
+  file_name <- paste(path_f, 'ds', "_",nset,"_e.csv", sep="")
+  ds_tmp = read.csv(file_name,check.names=FALSE, row.names=1)
+  c_sample = c_d_data$Sample[c_d_data$Group== 'c']
+  d_sample = c_d_data$Sample[c_d_data$Group== 'd']
+  ds_tmp$mean_c = rowMeans(as.data.frame(ds_tmp[ , c(c_sample)])[,-1])
+  ds_tmp$mean_d = rowMeans(as.data.frame(ds_tmp[ , c(d_sample)])[,-1])
+  #ds_tmp$p_value = apply(ds_tmp, 1, function(x) t.test(x[-3],x[-2])$p.value)
+  ds_tmp$p_value2 = apply(ds_tmp, 1, function(x) t.test(x[c(c_sample)],x[c(d_sample)])$p.value)
+  ds_tmp$genes = rownames(ds_tmp)
+  p_frame = as.data.frame(ds_tmp[ ,c('genes', 'p_value2') ])
+  colnames(p_frame) = c('genes', nset)
+  gsva_pval = merge(p_frame, gsva_pval, by= "genes",all = TRUE)
+  
+  
+}
+return(gsva_pval)
+}
+
+
+
+gsva_p = t_test_all_genes('Results/new_res/gsva/')
+plage_p = t_test_all_genes('Results/new_res/plage/')
+ssgsea_p = t_test_all_genes('Results/new_res/ssgsea/')
+
+write.csv(gsva_p, 'Results/new_res/gsva_all_genes.csv', row.names = TRUE)
+write.csv(plage_p, 'Results/new_res/plage_all_genes.csv', row.names = TRUE)
+write.csv(ssgsea_p, 'Results/new_res/ssgsea_all_genes.csv', row.names = TRUE)
+
+
+
+a = as.data.frame(a)
+a$mean_c = rowMeans(a[,-1])
+pValues <- apply(ds_tmp, 1, function(x) t.test(x[c(c_sample)],x[c(d_sample)])$p.value)
+
+t.test(ds_tmp$mean_c[1],ds_tmp$mean_d[1])
+
+
+
+h1 = ds_tmp[ds_tmp$p_value2 < 0.05, ]
+gsva_pval = rbind(gsva_pval,as.data.frame(ds_tmp[ , 'p_value2']))
+gsva_pval = merge(p_frame, gsva_pval, by= "genes",all.x = TRUE)
